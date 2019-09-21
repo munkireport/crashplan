888b1973b17efc071754be8149cf9c6bda235a2c
Arjen van Bochoven <nejra@me.com>
2019-09-21 18:36:48 +0200

    Add detail widget

--- provides.php
+++ provides.php
@@ -1,6 +1,9 @@
 <?php
 
 return array(
+    'detail_widgets' => [
+        'crashplan_detail' => ['view' => 'crashplan_detail_widget'],
+    ],
     'listings' => array(
         'crashplan' => array('view' => 'crashplan_listing', 'i18n' => 'crashplan.title'),
     ),
---
+++ views/crashplan_detail_widget.php
@@ -0,0 +1,5 @@
+<div class="col-lg-4">
+    <h4><i class="fa fa-home"></i> <span data-i18n="crashplan.title"></span></h4>
+    <table class="mr-crashplan-table">
+    </table>
+</div>
