d76c1d75fc254007e43089f079db9f62170b1b52
Mosen <mosen@users.noreply.github.com>
2021-05-22 17:13:11 +1000

    Bootstrap 4 Style Updates for MunkiReport-PHP v6.0.0 beta 1 Start using version numbers in dependent packages.

--- views/crashplan_listing.php
+++ views/crashplan_listing.php
@@ -4,13 +4,13 @@
 	)
 )); ?>
 
-<div class="container">
+<div class="container-fluid">
 
-  <div class="row">
+  <div class="row pt-4">
 
   	<div class="col-lg-12">
 
-		  <h3><span data-i18n="crashplan.listing_title"></span> <span id="total-count" class='label label-primary'>…</span></h3>
+		  <h3><span data-i18n="crashplan.listing_title"></span> <span id="total-count" class='badge badge-primary'>…</span></h3>
 
 		  <table class="table table-striped table-condensed table-bordered">
 		    <thead>
