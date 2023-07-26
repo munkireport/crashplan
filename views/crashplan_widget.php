e844c6d9ab0e039ca492e35e9e3c2b6e3d63c943
tuxudo <tuxudo@users.noreply.github.com>
2023-07-26 12:26:51 -0400

    Move v6 Styling

--- views/crashplan_widget.php
+++ views/crashplan_widget.php
@@ -1,53 +1,47 @@
 <div class="col-lg-4 col-md-6">
-    <div class="card">
-        <div class="card-header" data-container="body" title="">
+    <div class="card" id="crashplan-widget">
+        <div class="card-header" data-container="body">
             <i class="fa fa-clock-o"></i>
-            <span data-i18n="crashplan.widget_title"></span>
-            <a href="/show/listing/crashplan/crashplan" class="pull-right"><i class="fa fa-list"></i></a>
+                <span data-i18n="crashplan.widget_title"></span>
+                <a href="/show/listing/crashplan/crashplan" class="pull-right"><i class="fa fa-list"></i></a>
         </div>
-
-        <div class="list-group scroll-box">
-            <a id="cpp-today" href="<?= url('module/crashplan/listing') ?>"
-               class="list-group-item list-group-item-success hide">
-                <span class="badge">0</span>
-                <span data-i18n="backup.today"></span>
-            </a>
-            <a id="cpp-lastweek" href="<?= url('module/crashplan/listing') ?>"
-               class="list-group-item list-group-item-warning hide">
-                <span class="badge">0</span>
-                <span data-i18n="backup.lastweek"></span>
-            </a>
-            <a id="cpp-week_plus" href="<?= url('module/crashplan/listing') ?>"
-               class="list-group-item list-group-item-danger hide">
-                <span class="badge">0</span>
-                <span data-i18n="backup.week_plus"></span>
-            </a>
-            <span id="cpp-nodata" data-i18n="no_clients" class="list-group-item"></span>
-        </div>
-
+        <div class="card-body text-center"></div>
     </div><!-- /panel -->
-
 </div><!-- /col -->
 
 <script>
-  $(document).on('appReady appUpdate', function (e, lang) {
+$(document).on('appUpdate', function(e, lang) {
 
-    $.getJSON(appUrl + '/module/crashplan/get_stats', function (data) {
+    $.getJSON( appUrl + '/module/crashplan/get_stats', function( data ) {
 
-// Show no clients span
-      $('#cpp-nodata').removeClass('hide');
-
-      $.each(data, function (prop, val) {
-        if (val > 0) {
-          $('#cpp-' + prop).removeClass('hide');
-          $('#cpp-' + prop + ' > .badge').text(val);
-
-          // Hide no clients span
-          $('#cpp-nodata').addClass('hide');
-        } else {
-          $('#cpp-' + prop).addClass('hide');
+        if(data.error){
+            // alert(data.error);
+            return;
         }
-      });
+
+        var panel = $('#crashplan-widget div.card-body'),
+        baseUrl = appUrl + '/show/listing/crashplan/crashplan';
+        panel.empty();
+
+        // Set statuses
+        if(data.week_plus != "0"){
+            panel.append(' <a href="'+baseUrl+'" class="btn btn-danger"><span class="bigger-150">'+data.week_plus+'</span><br>'+i18n.t('backup.week_plus')+'</a>');
+        } else {
+            panel.append(' <a href="'+baseUrl+'" class="btn btn-danger disabled"><span class="bigger-150">'+data.week_plus+'</span><br>'+i18n.t('backup.week_plus')+'</a>');
+        }
+        
+        if(data.lastweek != "0"){
+            panel.append(' <a href="'+baseUrl+'" class="btn btn-warning"><span class="bigger-150">'+data.lastweek+'</span><br>'+i18n.t('backup.lastweek')+'</a>');
+        } else {
+            panel.append(' <a href="'+baseUrl+'" class="btn btn-warning disabled"><span class="bigger-150">'+data.lastweek+'</span><br>'+i18n.t('backup.lastweek')+'</a>');
+        }
+        
+        if(data.today != "0"){
+            panel.append(' <a href="'+baseUrl+'" class="btn btn-success"><span class="bigger-150">'+data.today+'</span><br>'+i18n.t('backup.today')+'</a>');
+        } else {
+            panel.append(' <a href="'+baseUrl+'" class="btn btn-success disabled"><span class="bigger-150">'+data.today+'</span><br>'+i18n.t('backup.today')+'</a>');
+        }
+
     });
-  });
+});
 </script>
